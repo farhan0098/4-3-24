15ad5455df29bceb2b9bbaf9c494dc96408d80a0
Farhan Ali <159815493+farhan0098@users.noreply.github.com>
2024-03-04 23:09:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// var firstName=prompt("enter your first name");
+// var lastName=prompt("enter your last name ");
+// var fullName=firstName+ " "+lastName
+
+//  function sum(){
+//     var num1=+prompt("enter your sum first");
+//     var num2=+prompt("enter your second ");
+//     var sum=num1+num2
+  
+//     console.log(sum)
+//  } 
+//  sum()
+//  sum()
+//  sum()
+//  sum()
+//  sum()
+// function sum(num1,num2,num3){
+//     console.log(num1,num2,num3)
+// }
+// sum(10,20,360)
+// function farhan(){
+//     var firstName=prompt("enter your fristname ");
+//     var lastName=prompt("enter your lastname");
+//     var fullName=firstName+" "+lastName;
+//     console.log(fullName)
+// }
+
+
+// farhan()
+
+ 
+
+
+
+// function good(){
+// var num1=prompt("enter value 1")
+// var num2=prompt("enter value 2")
+
+
+// }
